19dbd0b1a5459334419fd6ef3bffe4ecab6d8e38
sarvinshrivastava <41331485+sarvinshrivastava@users.noreply.github.com>
2022-10-03 19:00:51 +0530

    Create HackerRank-Program Between Two Sets (1).c

---
+++ HackerRank-Program Between Two Sets (1).c
@@ -0,0 +1,248 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+int getLCM(int, int);
+int getLC(int, int);
+/*
+ * Complete the 'getTotalX' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+int getTotalX(int a_count, int* a, int b_count, int* b) {
+        int i, j, k, count = 0, m = 0, lcm, p, q;
+        int* trt = calloc(5, sizeof(int));
+        if(a_count == 2) {
+                p = *(a); q = *(a + 1);
+                lcm = getLCM(p, q);
+        }
+        else {
+                p = *(a); q = *(a + 1);
+                lcm = getLCM(p, q);
+                for(i = 2; i < a_count; i++) {
+                        j = *(a + i);
+                        lcm = getLC(lcm, j);
+                }
+        }
+        //printf("%d %d", *(a + a_count - 1), *(b + 0));
+        printf("Temp list |\n");
+        for(k = *(a + a_count - 1); k <= *(b + 0); k++) {
+        //printf("%d\n", k);
+                if(k % lcm == 0) {
+                        *(trt + m) = k;
+                        printf("%d\n", *(trt + m));
+                        m++;
+                }
+        }
+        //j = sizeof(trt); // sizeof(int);
+        printf("Actual list|\n");
+        for(i = 0; i < m; i++){
+                for (k = 0; k < b_count; k++) {
+                                printf("i = %d k = %d\n",i, k);
+                                if((*(b + k) % *(trt + i)) != 0) {
+                                break;;
+                        }
+                        else {
+                                printf("%d\n", *(trt + i));
+                                if (k == b_count - 1) 
+                                        count++;
+                        }
+                }
+        }
+        printf("count = %d", count);
+        return count;
+}
+int getLCM(int p, int q) {
+    int m;  
+    m = (p > q) ? p : q;
+    while(true){
+        if(m % p == 0 && m % q == 0)
+            return m;
+        m++;
+    }
+}
+int getLC(int lcm, int j) {
+    int m;  
+    m = (lcm > j) ? lcm : j;
+    while(true){
+        if(m % lcm == 0 && m % j == 0)
+            return m;
+        m++;
+    }
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char** first_multiple_input = split_string(rtrim(readline()));
+
+    int n = parse_int(*(first_multiple_input + 0));
+
+    int m = parse_int(*(first_multiple_input + 1));
+
+    char** arr_temp = split_string(rtrim(readline()));
+
+    int* arr = malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = parse_int(*(arr_temp + i));
+
+        *(arr + i) = arr_item;
+    }
+
+    char** brr_temp = split_string(rtrim(readline()));
+
+    int* brr = malloc(m * sizeof(int));
+
+    for (int i = 0; i < m; i++) {
+        int brr_item = parse_int(*(brr_temp + i));
+
+        *(brr + i) = brr_item;
+    }
+
+    int total = getTotalX(n, arr, m, brr);
+
+    fprintf(fptr, "%d\n", total);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
