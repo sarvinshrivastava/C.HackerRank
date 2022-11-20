fe1fac1400d7606372488f9c631e09cad889e24f
sarvinshrivastava <41331485+sarvinshrivastava@users.noreply.github.com>
2022-11-20 16:01:25 +0530

    Structure and unions

---
+++ Time_struct with update.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+struct time_st {
+    int hour, min, sec;
+};
+
+void main() {
+    struct time_st tst;
+    scanf("%d%d%d", &tst.hour, &tst.min, &tst.sec);
+    tst.sec += 1;
+    if(tst.sec == 60) {
+        tst.sec = 00;
+        tst.min += 1;
+    }
+    if(tst.min == 60) {
+        tst.min = 00;
+        tst.hour += 1;
+    }
+    if(tst.hour == 24)
+        tst.hour = 00;
+    printf("%d:%d:%d", tst.hour, tst.min, tst.sec);
+}
